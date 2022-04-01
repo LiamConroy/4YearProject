bdc90843e34ef14932df36f7ccc66c751124232b
Liam Conroy (Student) <N00181800@iadt.ie>
2022-04-01 17:02:48 +0100

    position UI

--- SteelCavalry/Assets/Scripts/Buttons.cs
+++ SteelCavalry/Assets/Scripts/Buttons.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class Buttons : MonoBehaviour
 {
 
-    public GameObject panel;
+    
+    public GameObject pageOne;
+    public GameObject pageTwo;
+    public GameObject pageThree;
+
+    public Collider back;
+    public Collider next;
+
+    public int pageCount;
     // Start is called before the first frame update
-
-    public void setActive(){
-        panel.SetActive(true);
+    void start(){
+        pageCount = 1;
     }
 
-    public void setInactive(){
-        panel.SetActive(false);
+    void OnTriggerEnter(Collider back){
+        if(back.transform.CompareTag("GameController")){
+            Debug.Log("Back!");
+        }
+
+        // if(other.transform.CompareTag("GameController")){
+        //     Debug.Log("Next!");
+        // }
     }
+    
 
 
 }
--- SteelCavalry/Assets/Scripts/TargetHit.cs
+++ SteelCavalry/Assets/Scripts/TargetHit.cs
@@ -9,6 +9,8 @@ public class TargetHit : MonoBehaviour
     public GameObject projectile;
 
     public GameObject explosion;
+
+    public GameObject enemyHitExp;
     void Start()
     {
         
@@ -23,14 +25,20 @@ public class TargetHit : MonoBehaviour
      IEnumerator OnTriggerEnter(Collider other){
         if (other.transform.CompareTag("Enemy")){
             Debug.Log("Hit");
-            Destroy(projectile);      
+            projectile.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            projectile.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            // enemyHitExp.SetActive(true);
+
+            yield return new WaitForSeconds(1);
+            // Destroy(projectile);      
 
         }
 
         if (other.transform.CompareTag("Ground")){
-            explosion.SetActive(true);
-            yield return new WaitForSeconds(1);
-            Destroy(projectile);
+            // explosion.SetActive(true);
+            // yield return new WaitForSeconds(1);
+            Debug.Log("Ground Hit");
+            // Destroy(projectile);
         }
 
     }
